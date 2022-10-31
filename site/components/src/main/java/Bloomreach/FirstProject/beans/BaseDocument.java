6ce5a01e7b87eb8306d969a2ec299dd46ff5147a
Shikha Mudgal <shikha.mudgal93@gmail.com>
2022-10-31 21:52:12 +0530

    Initial commit

---
+++ site/components/src/main/java/Bloomreach/FirstProject/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package Bloomreach.FirstProject.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="FirstProject:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
